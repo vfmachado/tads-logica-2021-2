f34155b9b89ec3e055ecd30822c0a6c86f5dc045
Vinicius <vinicius.machado@sysaid.com.br>
2021-05-28 10:19:37 -0300

    Semana 02

---
+++ Semana02/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+
+  //MAIN => PRINCIPAL, PONTO DE PARTIDA!
+  public static void main(String[] args) {
+
+    //AQUI ESTA O MEU CODIGO, A MINHA SEQUENCIA DE PASSOS, AS MINHAS INTRUCOES
+    /*
+    COMENTARIO DE BLOCO
+    COMENTARIOS NAO SERAO COMPILADOS, JAMAIS, NUNCA, NEVER...
+    */
+    //MOSTRAR ALGO NA SAIDA PADRAO (TERMINAL)
+    System.out.println("Hello TADS Logica =)"); //line => adiciona um enter no final
+    System.out.println("    vamos comecar a programar?");
+
+    System.out.print("Teste com so print...");  //print (sem o ln) nao tem a quebra de linha
+    System.out.println("segundo print");
+
+    int a = 10;
+    System.out.println(a);
+
+  }
+
+
+}
